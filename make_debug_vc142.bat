c5910bf563db7540ae287704031d363c29c68b82
Nikolay <2012tafi2012@gmail.com>
2020-11-04 23:17:18 +0300

    fix

--- make_debug_vc142.bat
+++ make_debug_vc142.bat
@@ -3,7 +3,7 @@ qmake -tp vc -o .\src\Chart\ .\src\Chart\
 qmake -tp vc -o .\src\ElectronicComponent\ .\src\ElectronicComponent\
 qmake -tp vc -o .\src\GeneralAPI\ .\src\GeneralAPI\
 qmake -tp vc -o .\src\MainWindow\ .\src\MainWindow\ 
-qmake -tp vc -o .\src\ .\src\
+
 qmake -tp vc -o .\ElectricalCircuitBuildingSystem\ .\ElectricalCircuitBuildingSystem\
 
 qmake -tp vc
